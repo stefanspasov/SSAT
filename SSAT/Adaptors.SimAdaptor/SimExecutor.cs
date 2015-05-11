aff0691be463880e8f6548beffa947ab6473a982
DanielDHuynh <hthduy@gmail.com>
2015-05-11 14:16:24 +0200

    Follow up e00261f31a898a995f080d30e4e8ce77cac47667

---
+++ SSAT/Adaptors.SimAdaptor/SimExecutor.cs
@@ -0,0 +1,58 @@
+﻿using SATFUtilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEnvironment.Executors {
+    public class SimExecutor : IExecutor {
+        bool _started = false;
+        System.Diagnostics.Process _simProcess = null;
+        StreamWriter _streamWriter = null;
+        public string Execute(string source) {
+            if (source == "<stop>") {
+                ShutDown();
+                return "sim stopped";
+            }
+            if (source == "<start>" && _started == true) return "sim has been started already";
+            if (!_started) {
+                StartUp();
+                if (source == "<start>") return "sim started";
+            }
+            if (source.Contains('^')) {
+                _streamWriter.WriteLine(source.Split('^')[1]);
+                System.Threading.Thread.Sleep(Int32.Parse(source.Split('^')[0]));
+            } else {
+                _streamWriter.WriteLine(source);
+                System.Threading.Thread.Sleep(1000);
+            }
+            return "Done with the following command: " + source;
+        }
+
+        public void StartUp() {
+            ProcessStartInfo pi = new ProcessStartInfo();
+            pi.RedirectStandardInput = true;
+            pi.UseShellExecute = false;
+            pi.WorkingDirectory = Constants.ADEXP_SIM_PATH;
+            pi.FileName = Constants.ADEXP_SIM_PATH + "/ADEXPSim.exe";
+            _simProcess = System.Diagnostics.Process.Start(pi);
+            _streamWriter = _simProcess.StandardInput;
+            _streamWriter.WriteLine("call adexp_setup.txt");
+            System.Threading.Thread.Sleep(7000);
+            _started = true;
+        }
+
+        public void ShutDown() {
+            if (_started) {
+                _streamWriter.WriteLine("quit");
+                _streamWriter.WriteLine();
+                System.Threading.Thread.Sleep(500);
+                _streamWriter.Close();
+                _started = false;
+            }
+        }
+    }
+}
---
+++ SSAT/Adaptors.SimAdaptor/SimOrganizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SATFUtilities;
+using System.IO;
+using TestEnvironment.Entities;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+
+namespace TestEnvironment.Organizers {
+    public class SimOrganizer : IOrganizer {
+        public void Setup(IPAddress ipAddress) {
+            //TcpClient cs = new TcpClient();
+            //cs.Connect(ipAddress, Constants.RUN_SCRIPT_PORT);
+            //Connectivity.SendData(cs, JsonConvert.SerializeObject(new Operation("<start>", "Sim")));
+            //cs.Close();
+        }
+        public void TearDown(IPAddress ipAddress) {
+            //TcpClient cs = new TcpClient();
+            //cs.Connect(ipAddress, Constants.RUN_SCRIPT_PORT);
+            //Connectivity.SendData(cs, JsonConvert.SerializeObject(new Operation("<stop>", "Sim")));
+            //cs.Close();
+        }
+    }
+}
